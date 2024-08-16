rsodre/origami

added introspection::src5 to presets and mocks

token/src/components/tests/mocks/erc721/erc721_balance_mock.cairo
@@ -42,11 +42,13 @@ trait IERC721BalanceMockInit<TState> {
 #[dojo::contract]
 mod erc721_balance_mock {
     use starknet::ContractAddress;
+    use token::components::introspection::src5::src5_component;
     use token::components::token::erc721::erc721_approval::erc721_approval_component;
     use token::components::token::erc721::erc721_balance::erc721_balance_component;
     use token::components::token::erc721::erc721_mintable::erc721_mintable_component;
     use token::components::token::erc721::erc721_owner::erc721_owner_component;
 
+    component!(path: src5_component, storage: src5, event: SRC5Event);
     component!(
         path: erc721_approval_component, storage: erc721_approval, event: ERC721ApprovalEvent
     );
@@ -83,6 +85,8 @@ mod erc721_balance_mock {
     #[storage]
     struct Storage {
         #[substorage(v0)]
+        src5: src5_component::Storage,
+        #[substorage(v0)]
         erc721_approval: erc721_approval_component::Storage,
         #[substorage(v0)]
         erc721_balance: erc721_balance_component::Storage,
@@ -95,6 +99,7 @@ mod erc721_balance_mock {
     #[event]
     #[derive(Drop, starknet::Event)]
     enum Event {
+        SRC5Event: src5_component::Event,
         ERC721ApprovalEvent: erc721_approval_component::Event,
         ERC721BalanceEvent: erc721_balance_component::Event,
         ERC721MintableEvent: erc721_mintable_component::Event,

token/src/components/tests/mocks/erc721/erc721_enumerable_mock.cairo
@@ -21,11 +21,13 @@ trait IERC721EnumerableMock<TState> {
 mod erc721_enumerable_mock {
     use starknet::ContractAddress;
 
+    use token::components::introspection::src5::src5_component;
     use token::components::token::erc721::erc721_approval::erc721_approval_component;
     use token::components::token::erc721::erc721_balance::erc721_balance_component;
     use token::components::token::erc721::erc721_enumerable::erc721_enumerable_component;
     use token::components::token::erc721::erc721_owner::erc721_owner_component;
 
+    component!(path: src5_component, storage: src5, event: SRC5Event);
     component!(
         path: erc721_approval_component, storage: erc721_approval, event: ERC721ApprovalEvent
     );
@@ -35,6 +37,13 @@ mod erc721_enumerable_mock {
     );
     component!(path: erc721_owner_component, storage: erc721_owner, event: ERC721OwnerEvent);
 
+
+    #[abi(embed_v0)]
+    impl SRC5Impl =
+        src5_component::SRC5Impl<ContractState>;
+
+    #[abi(embed_v0)]
+        impl SRC5CamelImpl = src5_component::SRC5CamelImpl<ContractState>;
 
     #[abi(embed_v0)]
     impl ERC721ApprovalImpl =
@@ -71,6 +80,8 @@ mod erc721_enumerable_mock {
     #[storage]
     struct Storage {
         #[substorage(v0)]
+        src5: src5_component::Storage,
+        #[substorage(v0)]
         erc721_approval: erc721_approval_component::Storage,
         #[substorage(v0)]
         erc721_balance: erc721_balance_component::Storage,
@@ -83,6 +94,7 @@ mod erc721_enumerable_mock {
     #[event]
     #[derive(Drop, starknet::Event)]
     enum Event {
+        SRC5Event: src5_component::Event,
         ERC721ApprovalEvent: erc721_approval_component::Event,
         ERC721BalanceEvent: erc721_balance_component::Event,
         ERC721EnumerableEvent: erc721_enumerable_component::Event,

token/src/components/tests/mocks/erc721/erc721_metadata_hooks_mock.cairo
@@ -11,12 +11,14 @@ trait IERC721MetadataHooksMock<TContractState> {
 mod erc721_metadata_hooks_mock {
 
     use starknet::{get_contract_address};
+    use token::components::introspection::src5::src5_component;
     use token::components::token::erc721::erc721_approval::erc721_approval_component;
     use token::components::token::erc721::erc721_balance::erc721_balance_component;
     use token::components::token::erc721::erc721_metadata::erc721_metadata_component;
     use token::components::token::erc721::erc721_mintable::erc721_mintable_component;
     use token::components::token::erc721::erc721_owner::erc721_owner_component;
 
+    component!(path: src5_component, storage: src5, event: SRC5Event);
     component!(
         path: erc721_approval_component, storage: erc721_approval, event: ERC721ApprovalEvent
     );
@@ -30,8 +32,7 @@ mod erc721_metadata_hooks_mock {
     component!(path: erc721_owner_component, storage: erc721_owner, event: ERC721OwnerEvent);
 
     #[abi(embed_v0)]
-    impl ERC721MetadataImpl =
-        erc721_metadata_component::ERC721MetadataImpl<ContractState>;
+    impl ERC721MetadataImpl = erc721_metadata_component::ERC721MetadataImpl<ContractState>;
 
     impl ERC721ApprovalInternalImpl = erc721_approval_component::InternalImpl<ContractState>;
     impl ERC721BalanceInternalImpl = erc721_balance_component::InternalImpl<ContractState>;
@@ -41,6 +42,8 @@ mod erc721_metadata_hooks_mock {
 
     #[storage]
     struct Storage {
+        #[substorage(v0)]
+        src5: src5_component::Storage,
         #[substorage(v0)]
         erc721_approval: erc721_approval_component::Storage,
         #[substorage(v0)]
@@ -56,6 +59,7 @@ mod erc721_metadata_hooks_mock {
     #[event]
     #[derive(Drop, starknet::Event)]
     enum Event {
+        SRC5Event: src5_component::Event,
         ERC721ApprovalEvent: erc721_approval_component::Event,
         ERC721BalanceEvent: erc721_balance_component::Event,
         ERC721MetadataEvent: erc721_metadata_component::Event,

token/src/components/tests/mocks/erc721/erc721_metadata_mock.cairo
@@ -1,5 +1,6 @@
 #[dojo::contract]
 mod erc721_metadata_mock {
+    use token::components::introspection::src5::src5_component;
     use token::components::token::erc721::erc721_approval::erc721_approval_component;
     use token::components::token::erc721::erc721_balance::erc721_balance_component;
     use token::components::token::erc721::erc721_metadata::erc721_metadata_component;
@@ -7,6 +8,7 @@ mod erc721_metadata_mock {
     use token::components::token::erc721::erc721_mintable::erc721_mintable_component;
     use token::components::token::erc721::erc721_owner::erc721_owner_component;
 
+    component!(path: src5_component, storage: src5, event: SRC5Event);
     component!(
         path: erc721_approval_component, storage: erc721_approval, event: ERC721ApprovalEvent
     );
@@ -20,8 +22,7 @@ mod erc721_metadata_mock {
     component!(path: erc721_owner_component, storage: erc721_owner, event: ERC721OwnerEvent);
 
     #[abi(embed_v0)]
-    impl ERC721MetadataImpl =
-        erc721_metadata_component::ERC721MetadataImpl<ContractState>;
+    impl ERC721MetadataImpl = erc721_metadata_component::ERC721MetadataImpl<ContractState>;
 
     impl ERC721ApprovalInternalImpl = erc721_approval_component::InternalImpl<ContractState>;
     impl ERC721BalanceInternalImpl = erc721_balance_component::InternalImpl<ContractState>;
@@ -31,6 +32,8 @@ mod erc721_metadata_mock {
 
     #[storage]
     struct Storage {
+        #[substorage(v0)]
+        src5: src5_component::Storage,
         #[substorage(v0)]
         erc721_approval: erc721_approval_component::Storage,
         #[substorage(v0)]
@@ -46,6 +49,7 @@ mod erc721_metadata_mock {
     #[event]
     #[derive(Drop, starknet::Event)]
     enum Event {
+        SRC5Event: src5_component::Event,
         ERC721ApprovalEvent: erc721_approval_component::Event,
         ERC721BalanceEvent: erc721_balance_component::Event,
         ERC721MetadataEvent: erc721_metadata_component::Event,

token/src/components/tests/mocks/erc721/erc721_mintable_burnable_mock.cairo
@@ -1,5 +1,6 @@
 #[dojo::contract]
 mod erc721_mintable_burnable_mock {
+    use token::components::introspection::src5::src5_component;
     use token::components::token::erc721::erc721_approval::erc721_approval_component;
     use token::components::token::erc721::erc721_balance::erc721_balance_component;
     use token::components::token::erc721::erc721_metadata::erc721_metadata_component;
@@ -8,6 +9,7 @@ mod erc721_mintable_burnable_mock {
     use token::components::token::erc721::erc721_burnable::erc721_burnable_component;
     use token::components::token::erc721::erc721_owner::erc721_owner_component;
 
+    component!(path: src5_component, storage: src5, event: SRC5Event);
     component!(
         path: erc721_approval_component, storage: erc721_approval, event: ERC721ApprovalEvent
     );
@@ -33,6 +35,8 @@ mod erc721_mintable_burnable_mock {
     #[storage]
     struct Storage {
         #[substorage(v0)]
+        src5: src5_component::Storage,
+        #[substorage(v0)]
         erc721_approval: erc721_approval_component::Storage,
         #[substorage(v0)]
         erc721_balance: erc721_balance_component::Storage,
@@ -49,6 +53,7 @@ mod erc721_mintable_burnable_mock {
     #[event]
     #[derive(Drop, starknet::Event)]
     enum Event {
+        SRC5Event: src5_component::Event,
         ERC721ApprovalEvent: erc721_approval_component::Event,
         ERC721BalanceEvent: erc721_balance_component::Event,
         ERC721MetadataEvent: erc721_metadata_component::Event,

token/src/presets/erc721/enumerable_mintable_burnable.cairo
@@ -77,6 +77,7 @@ mod ERC721EnumMintBurn {
     use starknet::ContractAddress;
     use starknet::{get_contract_address, get_caller_address};
     use token::components::security::initializable::initializable_component;
+    use token::components::introspection::src5::src5_component;
     use token::components::token::erc721::erc721_approval::erc721_approval_component;
     use token::components::token::erc721::erc721_balance::erc721_balance_component;
     use token::components::token::erc721::erc721_burnable::erc721_burnable_component;
@@ -88,6 +89,7 @@ mod ERC721EnumMintBurn {
 
     component!(path: initializable_component, storage: initializable, event: InitializableEvent);
 
+    component!(path: src5_component, storage: src5, event: SRC5Event);
     component!(
         path: erc721_approval_component, storage: erc721_approval, event: ERC721ApprovalEvent
     );
@@ -107,6 +109,9 @@ mod ERC721EnumMintBurn {
     component!(path: erc721_owner_component, storage: erc721_owner, event: ERC721OwnerEvent);
 
     impl InitializableImpl = initializable_component::InitializableImpl<ContractState>;
+
+    #[abi(embed_v0)]
+    impl SRC5Impl = src5_component::SRC5Impl<ContractState>;
 
     #[abi(embed_v0)]
     impl ERC721ApprovalImpl =
@@ -156,6 +161,8 @@ mod ERC721EnumMintBurn {
     #[storage]
     struct Storage {
         #[substorage(v0)]
+        src5: src5_component::Storage,
+        #[substorage(v0)]
         initializable: initializable_component::Storage,
         #[substorage(v0)]
         erc721_approval: erc721_approval_component::Storage,
@@ -176,6 +183,8 @@ mod ERC721EnumMintBurn {
     #[event]
     #[derive(Drop, starknet::Event)]
     enum Event {
+        #[flat]
+        SRC5Event: src5_component::Event,
         #[flat]
         InitializableEvent: initializable_component::Event,
         #[flat]

token/src/presets/erc721/mintable_burnable.cairo
@@ -57,6 +57,7 @@ mod ERC721MintableBurnable {
     use starknet::ContractAddress;
     use starknet::{get_caller_address, get_contract_address};
     use token::components::security::initializable::initializable_component;
+    use token::components::introspection::src5::src5_component;
     use token::components::token::erc721::erc721_approval::erc721_approval_component;
     use token::components::token::erc721::erc721_balance::erc721_balance_component;
     use token::components::token::erc721::erc721_burnable::erc721_burnable_component;
@@ -67,6 +68,7 @@ mod ERC721MintableBurnable {
 
     component!(path: initializable_component, storage: initializable, event: InitializableEvent);
 
+    component!(path: src5_component, storage: src5, event: SRC5Event);
     component!(
         path: erc721_approval_component, storage: erc721_approval, event: ERC721ApprovalEvent
     );
@@ -83,6 +85,9 @@ mod ERC721MintableBurnable {
     component!(path: erc721_owner_component, storage: erc721_owner, event: ERC721OwnerEvent);
 
     impl InitializableImpl = initializable_component::InitializableImpl<ContractState>;
+
+    #[abi(embed_v0)]
+    impl SRC5Impl = src5_component::SRC5Impl<ContractState>;
 
     #[abi(embed_v0)]
     impl ERC721ApprovalImpl =
@@ -122,6 +127,8 @@ mod ERC721MintableBurnable {
     #[storage]
     struct Storage {
         #[substorage(v0)]
+        src5: src5_component::Storage,
+        #[substorage(v0)]
         initializable: initializable_component::Storage,
         #[substorage(v0)]
         erc721_approval: erc721_approval_component::Storage,
@@ -140,6 +147,8 @@ mod ERC721MintableBurnable {
     #[event]
     #[derive(Drop, starknet::Event)]
     enum Event {
+        #[flat]
+        SRC5Event: src5_component::Event,
         #[flat]
         InitializableEvent: initializable_component::Event,
         #[flat]

Matth26/origami-roger

crates/token/src/components/tests/mocks/erc721/erc721_balance_mock.cairo
@@ -42,11 +42,13 @@ trait IERC721BalanceMockInit<TState> {
 #[dojo::contract]
 mod erc721_balance_mock {
     use starknet::ContractAddress;
+    use origami_token::components::introspection::src5::src5_component;
     use origami_token::components::token::erc721::erc721_approval::erc721_approval_component;
     use origami_token::components::token::erc721::erc721_balance::erc721_balance_component;
     use origami_token::components::token::erc721::erc721_mintable::erc721_mintable_component;
     use origami_token::components::token::erc721::erc721_owner::erc721_owner_component;
 
+    component!(path: src5_component, storage: src5, event: SRC5Event);
     component!(
         path: erc721_approval_component, storage: erc721_approval, event: ERC721ApprovalEvent
     );
@@ -83,6 +85,8 @@ mod erc721_balance_mock {
     #[storage]
     struct Storage {
         #[substorage(v0)]
+        src5: src5_component::Storage,
+        #[substorage(v0)]
         erc721_approval: erc721_approval_component::Storage,
         #[substorage(v0)]
         erc721_balance: erc721_balance_component::Storage,
@@ -95,6 +99,7 @@ mod erc721_balance_mock {
     #[event]
     #[derive(Drop, starknet::Event)]
     enum Event {
+        SRC5Event: src5_component::Event,
         ERC721ApprovalEvent: erc721_approval_component::Event,
         ERC721BalanceEvent: erc721_balance_component::Event,
         ERC721MintableEvent: erc721_mintable_component::Event,

crates/token/src/components/tests/mocks/erc721/erc721_enumerable_mock.cairo
@@ -21,11 +21,13 @@ trait IERC721EnumerableMock<TState> {
 mod erc721_enumerable_mock {
     use starknet::ContractAddress;
 
+    use origami_token::components::introspection::src5::src5_component;
     use origami_token::components::token::erc721::erc721_approval::erc721_approval_component;
     use origami_token::components::token::erc721::erc721_balance::erc721_balance_component;
     use origami_token::components::token::erc721::erc721_enumerable::erc721_enumerable_component;
     use origami_token::components::token::erc721::erc721_owner::erc721_owner_component;
 
+    component!(path: src5_component, storage: src5, event: SRC5Event);
     component!(
         path: erc721_approval_component, storage: erc721_approval, event: ERC721ApprovalEvent
     );
@@ -35,6 +37,13 @@ mod erc721_enumerable_mock {
     );
     component!(path: erc721_owner_component, storage: erc721_owner, event: ERC721OwnerEvent);
 
+
+    #[abi(embed_v0)]
+    impl SRC5Impl =
+        src5_component::SRC5Impl<ContractState>;
+
+    #[abi(embed_v0)]
+        impl SRC5CamelImpl = src5_component::SRC5CamelImpl<ContractState>;
 
     #[abi(embed_v0)]
     impl ERC721ApprovalImpl =
@@ -71,6 +80,8 @@ mod erc721_enumerable_mock {
     #[storage]
     struct Storage {
         #[substorage(v0)]
+        src5: src5_component::Storage,
+        #[substorage(v0)]
         erc721_approval: erc721_approval_component::Storage,
         #[substorage(v0)]
         erc721_balance: erc721_balance_component::Storage,
@@ -83,6 +94,7 @@ mod erc721_enumerable_mock {
     #[event]
     #[derive(Drop, starknet::Event)]
     enum Event {
+        SRC5Event: src5_component::Event,
         ERC721ApprovalEvent: erc721_approval_component::Event,
         ERC721BalanceEvent: erc721_balance_component::Event,
         ERC721EnumerableEvent: erc721_enumerable_component::Event,

crates/token/src/components/tests/mocks/erc721/erc721_metadata_hooks_mock.cairo
@@ -11,12 +11,14 @@ trait IERC721MetadataHooksMock<TContractState> {
 mod erc721_metadata_hooks_mock {
 
     use starknet::{get_contract_address};
+    use origami_token::components::introspection::src5::src5_component;
     use origami_token::components::token::erc721::erc721_approval::erc721_approval_component;
     use origami_token::components::token::erc721::erc721_balance::erc721_balance_component;
     use origami_token::components::token::erc721::erc721_metadata::erc721_metadata_component;
     use origami_token::components::token::erc721::erc721_mintable::erc721_mintable_component;
     use origami_token::components::token::erc721::erc721_owner::erc721_owner_component;
 
+    component!(path: src5_component, storage: src5, event: SRC5Event);
     component!(
         path: erc721_approval_component, storage: erc721_approval, event: ERC721ApprovalEvent
     );
@@ -30,8 +32,7 @@ mod erc721_metadata_hooks_mock {
     component!(path: erc721_owner_component, storage: erc721_owner, event: ERC721OwnerEvent);
 
     #[abi(embed_v0)]
-    impl ERC721MetadataImpl =
-        erc721_metadata_component::ERC721MetadataImpl<ContractState>;
+    impl ERC721MetadataImpl = erc721_metadata_component::ERC721MetadataImpl<ContractState>;
 
     impl ERC721ApprovalInternalImpl = erc721_approval_component::InternalImpl<ContractState>;
     impl ERC721BalanceInternalImpl = erc721_balance_component::InternalImpl<ContractState>;
@@ -41,6 +42,8 @@ mod erc721_metadata_hooks_mock {
 
     #[storage]
     struct Storage {
+        #[substorage(v0)]
+        src5: src5_component::Storage,
         #[substorage(v0)]
         erc721_approval: erc721_approval_component::Storage,
         #[substorage(v0)]
@@ -56,6 +59,7 @@ mod erc721_metadata_hooks_mock {
     #[event]
     #[derive(Drop, starknet::Event)]
     enum Event {
+        SRC5Event: src5_component::Event,
         ERC721ApprovalEvent: erc721_approval_component::Event,
         ERC721BalanceEvent: erc721_balance_component::Event,
         ERC721MetadataEvent: erc721_metadata_component::Event,

crates/token/src/components/tests/mocks/erc721/erc721_metadata_mock.cairo
@@ -1,5 +1,6 @@
 #[dojo::contract]
 mod erc721_metadata_mock {
+    use origami_token::components::introspection::src5::src5_component;
     use origami_token::components::token::erc721::erc721_approval::erc721_approval_component;
     use origami_token::components::token::erc721::erc721_balance::erc721_balance_component;
     use origami_token::components::token::erc721::erc721_metadata::erc721_metadata_component;
@@ -7,6 +8,7 @@ mod erc721_metadata_mock {
     use origami_token::components::token::erc721::erc721_mintable::erc721_mintable_component;
     use origami_token::components::token::erc721::erc721_owner::erc721_owner_component;
 
+    component!(path: src5_component, storage: src5, event: SRC5Event);
     component!(
         path: erc721_approval_component, storage: erc721_approval, event: ERC721ApprovalEvent
     );
@@ -20,8 +22,7 @@ mod erc721_metadata_mock {
     component!(path: erc721_owner_component, storage: erc721_owner, event: ERC721OwnerEvent);
 
     #[abi(embed_v0)]
-    impl ERC721MetadataImpl =
-        erc721_metadata_component::ERC721MetadataImpl<ContractState>;
+    impl ERC721MetadataImpl = erc721_metadata_component::ERC721MetadataImpl<ContractState>;
 
     impl ERC721ApprovalInternalImpl = erc721_approval_component::InternalImpl<ContractState>;
     impl ERC721BalanceInternalImpl = erc721_balance_component::InternalImpl<ContractState>;
@@ -31,6 +32,8 @@ mod erc721_metadata_mock {
 
     #[storage]
     struct Storage {
+        #[substorage(v0)]
+        src5: src5_component::Storage,
         #[substorage(v0)]
         erc721_approval: erc721_approval_component::Storage,
         #[substorage(v0)]
@@ -46,6 +49,7 @@ mod erc721_metadata_mock {
     #[event]
     #[derive(Drop, starknet::Event)]
     enum Event {
+        SRC5Event: src5_component::Event,
         ERC721ApprovalEvent: erc721_approval_component::Event,
         ERC721BalanceEvent: erc721_balance_component::Event,
         ERC721MetadataEvent: erc721_metadata_component::Event,

crates/token/src/components/tests/mocks/erc721/erc721_mintable_burnable_mock.cairo
@@ -1,5 +1,6 @@
 #[dojo::contract]
 mod erc721_mintable_burnable_mock {
+    use origami_token::components::introspection::src5::src5_component;
     use origami_token::components::token::erc721::erc721_approval::erc721_approval_component;
     use origami_token::components::token::erc721::erc721_balance::erc721_balance_component;
     use origami_token::components::token::erc721::erc721_metadata::erc721_metadata_component;
@@ -8,6 +9,7 @@ mod erc721_mintable_burnable_mock {
     use origami_token::components::token::erc721::erc721_burnable::erc721_burnable_component;
     use origami_token::components::token::erc721::erc721_owner::erc721_owner_component;
 
+    component!(path: src5_component, storage: src5, event: SRC5Event);
     component!(
         path: erc721_approval_component, storage: erc721_approval, event: ERC721ApprovalEvent
     );
@@ -33,6 +35,8 @@ mod erc721_mintable_burnable_mock {
     #[storage]
     struct Storage {
         #[substorage(v0)]
+        src5: src5_component::Storage,
+        #[substorage(v0)]
         erc721_approval: erc721_approval_component::Storage,
         #[substorage(v0)]
         erc721_balance: erc721_balance_component::Storage,
@@ -49,6 +53,7 @@ mod erc721_mintable_burnable_mock {
     #[event]
     #[derive(Drop, starknet::Event)]
     enum Event {
+        SRC5Event: src5_component::Event,
         ERC721ApprovalEvent: erc721_approval_component::Event,
         ERC721BalanceEvent: erc721_balance_component::Event,
         ERC721MetadataEvent: erc721_metadata_component::Event,

crates/token/src/presets/erc721/enumerable_mintable_burnable.cairo
@@ -77,6 +77,7 @@ mod ERC721EnumMintBurn {
     use starknet::ContractAddress;
     use starknet::{get_contract_address, get_caller_address};
     use origami_token::components::security::initializable::initializable_component;
+    use origami_token::components::introspection::src5::src5_component;
     use origami_token::components::token::erc721::erc721_approval::erc721_approval_component;
     use origami_token::components::token::erc721::erc721_balance::erc721_balance_component;
     use origami_token::components::token::erc721::erc721_burnable::erc721_burnable_component;
@@ -88,6 +89,7 @@ mod ERC721EnumMintBurn {
 
     component!(path: initializable_component, storage: initializable, event: InitializableEvent);
 
+    component!(path: src5_component, storage: src5, event: SRC5Event);
     component!(
         path: erc721_approval_component, storage: erc721_approval, event: ERC721ApprovalEvent
     );
@@ -107,6 +109,9 @@ mod ERC721EnumMintBurn {
     component!(path: erc721_owner_component, storage: erc721_owner, event: ERC721OwnerEvent);
 
     impl InitializableImpl = initializable_component::InitializableImpl<ContractState>;
+
+    #[abi(embed_v0)]
+    impl SRC5Impl = src5_component::SRC5Impl<ContractState>;
 
     #[abi(embed_v0)]
     impl ERC721ApprovalImpl =
@@ -156,6 +161,8 @@ mod ERC721EnumMintBurn {
     #[storage]
     struct Storage {
         #[substorage(v0)]
+        src5: src5_component::Storage,
+        #[substorage(v0)]
         initializable: initializable_component::Storage,
         #[substorage(v0)]
         erc721_approval: erc721_approval_component::Storage,
@@ -176,6 +183,8 @@ mod ERC721EnumMintBurn {
     #[event]
     #[derive(Drop, starknet::Event)]
     enum Event {
+        #[flat]
+        SRC5Event: src5_component::Event,
         #[flat]
         InitializableEvent: initializable_component::Event,
         #[flat]

crates/token/src/presets/erc721/mintable_burnable.cairo
@@ -57,6 +57,7 @@ mod ERC721MintableBurnable {
     use starknet::ContractAddress;
     use starknet::{get_caller_address, get_contract_address};
     use origami_token::components::security::initializable::initializable_component;
+    use origami_token::components::introspection::src5::src5_component;
     use origami_token::components::token::erc721::erc721_approval::erc721_approval_component;
     use origami_token::components::token::erc721::erc721_balance::erc721_balance_component;
     use origami_token::components::token::erc721::erc721_burnable::erc721_burnable_component;
@@ -67,6 +68,7 @@ mod ERC721MintableBurnable {
 
     component!(path: initializable_component, storage: initializable, event: InitializableEvent);
 
+    component!(path: src5_component, storage: src5, event: SRC5Event);
     component!(
         path: erc721_approval_component, storage: erc721_approval, event: ERC721ApprovalEvent
     );
@@ -83,6 +85,9 @@ mod ERC721MintableBurnable {
     component!(path: erc721_owner_component, storage: erc721_owner, event: ERC721OwnerEvent);
 
     impl InitializableImpl = initializable_component::InitializableImpl<ContractState>;
+
+    #[abi(embed_v0)]
+    impl SRC5Impl = src5_component::SRC5Impl<ContractState>;
 
     #[abi(embed_v0)]
     impl ERC721ApprovalImpl =
@@ -122,6 +127,8 @@ mod ERC721MintableBurnable {
     #[storage]
     struct Storage {
         #[substorage(v0)]
+        src5: src5_component::Storage,
+        #[substorage(v0)]
         initializable: initializable_component::Storage,
         #[substorage(v0)]
         erc721_approval: erc721_approval_component::Storage,
@@ -140,6 +147,8 @@ mod ERC721MintableBurnable {
     #[event]
     #[derive(Drop, starknet::Event)]
     enum Event {
+        #[flat]
+        SRC5Event: src5_component::Event,
         #[flat]
         InitializableEvent: initializable_component::Event,
         #[flat]