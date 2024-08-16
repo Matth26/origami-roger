dojoengine/origami

added metadata hooks

token/src/components/tests/mocks/erc721/erc721_mintable_burnable_mock.cairo
@@ -3,6 +3,7 @@ mod erc721_mintable_burnable_mock {
     use token::components::token::erc721::erc721_approval::erc721_approval_component;
     use token::components::token::erc721::erc721_balance::erc721_balance_component;
     use token::components::token::erc721::erc721_metadata::erc721_metadata_component;
+    use token::components::token::erc721::erc721_metadata_hooks::ERC721MetadataHooksEmptyImpl;
     use token::components::token::erc721::erc721_mintable::erc721_mintable_component;
     use token::components::token::erc721::erc721_burnable::erc721_burnable_component;
     use token::components::token::erc721::erc721_owner::erc721_owner_component;

token/src/components/tests/token/erc721/test_erc721_metadata_hooks.cairo
@@ -1,0 +1,41 @@
+use integer::BoundedInt;
+use dojo::world::{IWorldDispatcher, IWorldDispatcherTrait};
+use dojo::test_utils::spawn_test_world;
+use token::tests::constants::{OWNER};
+
+use token::components::token::erc721::erc721_metadata::{erc_721_meta_model, ERC721MetaModel,};
+use token::components::token::erc721::erc721_metadata::erc721_metadata_component::{
+    ERC721MetadataImpl, ERC721MetadataCamelImpl, InternalImpl
+};
+use token::components::token::erc721::erc721_mintable::erc721_mintable_component::InternalImpl as ERC721MintableInternalImpl;
+
+use token::components::tests::mocks::erc721::erc721_metadata_hooks_mock::erc721_metadata_hooks_mock;
+use starknet::storage::{StorageMemberAccessTrait};
+
+
+fn STATE() -> (IWorldDispatcher, erc721_metadata_hooks_mock::ContractState) {
+    let world = spawn_test_world(array![erc_721_meta_model::TEST_CLASS_HASH,]);
+
+    let mut state = erc721_metadata_hooks_mock::contract_state_for_testing();
+    state.world_dispatcher.write(world);
+
+    (world, state)
+}
+
+#[test]
+fn test_erc721_metadata_hooks_initialize() {
+    let (_world, mut state) = STATE();
+
+    let NAME: ByteArray = "NAME";
+    let SYMBOL: ByteArray = "SYMBOL";
+    let URI: ByteArray = "URI";
+
+    state.erc721_metadata.initialize(NAME, SYMBOL, URI);
+
+    assert(state.erc721_metadata.name() == "NAME", 'Should be NAME');
+    assert(state.erc721_metadata.symbol() == "SYMBOL", 'Should be SYMBOL');
+
+    state.erc721_mintable.mint(OWNER(), 1);
+    assert(state.erc721_metadata.token_uri(1) == "CUSTOMURI1", 'Should be CUSTOMURI1');
+    assert(state.erc721_metadata.tokenURI(1) == "CUSTOMURI1", 'Should be CUSTOMURI1');
+}

token/src/components/token/erc721/erc721_metadata_hooks.cairo
@@ -1,0 +1,20 @@
+
+/// 
+/// An empty implementation of the ERC721MetadataHooksTrait
+/// 
+/// When the hook is not required, import together with the component:
+/// use token::components::token::erc721::erc721_metadata::erc721_metadata_component;
+/// use token::components::token::erc721::erc721_metadata_hooks::ERC721MetadataHooksEmptyImpl;
+/// 
+/// Or implement your own (example on erc721_metadata_hooks_mock.cairo)
+/// 
+
+use token::components::token::erc721::erc721_metadata::erc721_metadata_component;
+
+impl ERC721MetadataHooksEmptyImpl<TContractState> of erc721_metadata_component::ERC721MetadataHooksTrait<TContractState> {
+    fn custom_uri(
+        self: @erc721_metadata_component::ComponentState<TContractState>,
+        base_uri: @ByteArray,
+        token_id: u256,
+    ) -> ByteArray { "" }
+}

token/src/presets/erc721/enumerable_mintable_burnable.cairo
@@ -82,6 +82,7 @@ mod ERC721EnumMintBurn {
     use token::components::token::erc721::erc721_burnable::erc721_burnable_component;
     use token::components::token::erc721::erc721_enumerable::erc721_enumerable_component;
     use token::components::token::erc721::erc721_metadata::erc721_metadata_component;
+    use token::components::token::erc721::erc721_metadata_hooks::ERC721MetadataHooksEmptyImpl;
     use token::components::token::erc721::erc721_mintable::erc721_mintable_component;
     use token::components::token::erc721::erc721_owner::erc721_owner_component;
 

token/src/presets/erc721/mintable_burnable.cairo
@@ -61,6 +61,7 @@ mod ERC721MintableBurnable {
     use token::components::token::erc721::erc721_balance::erc721_balance_component;
     use token::components::token::erc721::erc721_burnable::erc721_burnable_component;
     use token::components::token::erc721::erc721_metadata::erc721_metadata_component;
+    use token::components::token::erc721::erc721_metadata_hooks::ERC721MetadataHooksEmptyImpl;
     use token::components::token::erc721::erc721_mintable::erc721_mintable_component;
     use token::components::token::erc721::erc721_owner::erc721_owner_component;
 

Matth26/origami-roger

crates/token/src/components/tests/mocks/erc721/erc721_mintable_burnable_mock.cairo
@@ -3,6 +3,7 @@ mod erc721_mintable_burnable_mock {
     use origami_token::components::token::erc721::erc721_approval::erc721_approval_component;
     use origami_token::components::token::erc721::erc721_balance::erc721_balance_component;
     use origami_token::components::token::erc721::erc721_metadata::erc721_metadata_component;
+    use origami_token::components::token::erc721::erc721_metadata_hooks::ERC721MetadataHooksEmptyImpl;
     use origami_token::components::token::erc721::erc721_mintable::erc721_mintable_component;
     use origami_token::components::token::erc721::erc721_burnable::erc721_burnable_component;
     use origami_token::components::token::erc721::erc721_owner::erc721_owner_component;

crates/token/src/components/tests/token/erc721/test_erc721_metadata_hooks.cairo
@@ -1,0 +1,41 @@
+use integer::BoundedInt;
+use dojo::world::{IWorldDispatcher, IWorldDispatcherTrait};
+use dojo::test_utils::spawn_test_world;
+use origami_token::tests::constants::{OWNER};
+
+use origami_token::components::token::erc721::erc721_metadata::{erc_721_meta_model, ERC721MetaModel,};
+use origami_token::components::token::erc721::erc721_metadata::erc721_metadata_component::{
+    ERC721MetadataImpl, ERC721MetadataCamelImpl, InternalImpl
+};
+use origami_token::components::token::erc721::erc721_mintable::erc721_mintable_component::InternalImpl as ERC721MintableInternalImpl;
+
+use origami_token::components::tests::mocks::erc721::erc721_metadata_hooks_mock::erc721_metadata_hooks_mock;
+use starknet::storage::{StorageMemberAccessTrait};
+
+
+fn STATE() -> (IWorldDispatcher, erc721_metadata_hooks_mock::ContractState) {
+    let world = spawn_test_world(array![erc_721_meta_model::TEST_CLASS_HASH,]);
+
+    let mut state = erc721_metadata_hooks_mock::contract_state_for_testing();
+    state.world_dispatcher.write(world);
+
+    (world, state)
+}
+
+#[test]
+fn test_erc721_metadata_hooks_initialize() {
+    let (_world, mut state) = STATE();
+
+    let NAME: ByteArray = "NAME";
+    let SYMBOL: ByteArray = "SYMBOL";
+    let URI: ByteArray = "URI";
+
+    state.erc721_metadata.initialize(NAME, SYMBOL, URI);
+
+    assert(state.erc721_metadata.name() == "NAME", 'Should be NAME');
+    assert(state.erc721_metadata.symbol() == "SYMBOL", 'Should be SYMBOL');
+
+    state.erc721_mintable.mint(OWNER(), 1);
+    assert(state.erc721_metadata.token_uri(1) == "CUSTOMURI1", 'Should be CUSTOMURI1');
+    assert(state.erc721_metadata.tokenURI(1) == "CUSTOMURI1", 'Should be CUSTOMURI1');
+}

crates/token/src/components/token/erc721/erc721_metadata_hooks.cairo
@@ -1,0 +1,20 @@
+
+/// 
+/// An empty implementation of the ERC721MetadataHooksTrait
+/// 
+/// When the hook is not required, import together with the component:
+/// use origami_token::components::token::erc721::erc721_metadata::erc721_metadata_component;
+/// use origami_token::components::token::erc721::erc721_metadata_hooks::ERC721MetadataHooksEmptyImpl;
+/// 
+/// Or implement your own (example on erc721_metadata_hooks_mock.cairo)
+/// 
+
+use origami_token::components::token::erc721::erc721_metadata::erc721_metadata_component;
+
+impl ERC721MetadataHooksEmptyImpl<TContractState> of erc721_metadata_component::ERC721MetadataHooksTrait<TContractState> {
+    fn custom_uri(
+        self: @erc721_metadata_component::ComponentState<TContractState>,
+        base_uri: @ByteArray,
+        token_id: u256,
+    ) -> ByteArray { "" }
+}

crates/token/src/presets/erc721/enumerable_mintable_burnable.cairo
@@ -82,6 +82,7 @@ mod ERC721EnumMintBurn {
     use origami_token::components::token::erc721::erc721_burnable::erc721_burnable_component;
     use origami_token::components::token::erc721::erc721_enumerable::erc721_enumerable_component;
     use origami_token::components::token::erc721::erc721_metadata::erc721_metadata_component;
+    use origami_token::components::token::erc721::erc721_metadata_hooks::ERC721MetadataHooksEmptyImpl;
     use origami_token::components::token::erc721::erc721_mintable::erc721_mintable_component;
     use origami_token::components::token::erc721::erc721_owner::erc721_owner_component;
 

crates/token/src/presets/erc721/mintable_burnable.cairo
@@ -61,6 +61,7 @@ mod ERC721MintableBurnable {
     use origami_token::components::token::erc721::erc721_balance::erc721_balance_component;
     use origami_token::components::token::erc721::erc721_burnable::erc721_burnable_component;
     use origami_token::components::token::erc721::erc721_metadata::erc721_metadata_component;
+    use origami_token::components::token::erc721::erc721_metadata_hooks::ERC721MetadataHooksEmptyImpl;
     use origami_token::components::token::erc721::erc721_mintable::erc721_mintable_component;
     use origami_token::components::token::erc721::erc721_owner::erc721_owner_component;
 