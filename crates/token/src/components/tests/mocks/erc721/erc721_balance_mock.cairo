rsodre/origami

moved contract enumerable logic to erc721_enumerable

token/src/components/tests/mocks/erc721/erc721_balance_mock.cairo
@@ -46,6 +46,7 @@ mod erc721_balance_mock {
     use token::components::token::erc721::erc721_approval::erc721_approval_component;
     use token::components::token::erc721::erc721_balance::erc721_balance_component;
     use token::components::token::erc721::erc721_mintable::erc721_mintable_component;
+    use token::components::token::erc721::erc721_enumerable::erc721_enumerable_component;
     use token::components::token::erc721::erc721_owner::erc721_owner_component;
 
     component!(path: src5_component, storage: src5, event: SRC5Event);
@@ -55,6 +56,9 @@ mod erc721_balance_mock {
     component!(path: erc721_balance_component, storage: erc721_balance, event: ERC721BalanceEvent);
     component!(
         path: erc721_mintable_component, storage: erc721_mintable, event: ERC721MintableEvent
+    );
+    component!(
+        path: erc721_enumerable_component, storage: erc721_enumerable, event: ERC721EnumerableEvent
     );
     component!(path: erc721_owner_component, storage: erc721_owner, event: ERC721OwnerEvent);
 
@@ -93,6 +97,8 @@ mod erc721_balance_mock {
         #[substorage(v0)]
         erc721_mintable: erc721_mintable_component::Storage,
         #[substorage(v0)]
+        erc721_enumerable: erc721_enumerable_component::Storage,
+        #[substorage(v0)]
         erc721_owner: erc721_owner_component::Storage,
     }
 
@@ -103,6 +109,7 @@ mod erc721_balance_mock {
         ERC721ApprovalEvent: erc721_approval_component::Event,
         ERC721BalanceEvent: erc721_balance_component::Event,
         ERC721MintableEvent: erc721_mintable_component::Event,
+        ERC721EnumerableEvent: erc721_enumerable_component::Event,
         ERC721OwnerEvent: erc721_owner_component::Event,
     }
 

token/src/components/tests/mocks/erc721/erc721_metadata_hooks_mock.cairo
@@ -16,6 +16,7 @@ mod erc721_metadata_hooks_mock {
     use token::components::token::erc721::erc721_balance::erc721_balance_component;
     use token::components::token::erc721::erc721_metadata::erc721_metadata_component;
     use token::components::token::erc721::erc721_mintable::erc721_mintable_component;
+    use token::components::token::erc721::erc721_enumerable::erc721_enumerable_component;
     use token::components::token::erc721::erc721_owner::erc721_owner_component;
 
     component!(path: src5_component, storage: src5, event: SRC5Event);
@@ -28,6 +29,9 @@ mod erc721_metadata_hooks_mock {
     );
     component!(
         path: erc721_mintable_component, storage: erc721_mintable, event: ERC721MintableEvent
+    );
+    component!(
+        path: erc721_enumerable_component, storage: erc721_enumerable, event: ERC721EnumerableEvent
     );
     component!(path: erc721_owner_component, storage: erc721_owner, event: ERC721OwnerEvent);
 
@@ -53,6 +57,8 @@ mod erc721_metadata_hooks_mock {
         #[substorage(v0)]
         erc721_mintable: erc721_mintable_component::Storage,
         #[substorage(v0)]
+        erc721_enumerable: erc721_enumerable_component::Storage,
+        #[substorage(v0)]
         erc721_owner: erc721_owner_component::Storage,
     }
 
@@ -64,6 +70,7 @@ mod erc721_metadata_hooks_mock {
         ERC721BalanceEvent: erc721_balance_component::Event,
         ERC721MetadataEvent: erc721_metadata_component::Event,
         ERC721MintableEvent: erc721_mintable_component::Event,
+        ERC721EnumerableEvent: erc721_enumerable_component::Event,
         ERC721OwnerEvent: erc721_owner_component::Event
     }
 

token/src/components/tests/mocks/erc721/erc721_metadata_mock.cairo
@@ -6,6 +6,7 @@ mod erc721_metadata_mock {
     use token::components::token::erc721::erc721_metadata::erc721_metadata_component;
     use token::components::token::erc721::erc721_metadata_hooks::ERC721MetadataHooksEmptyImpl;
     use token::components::token::erc721::erc721_mintable::erc721_mintable_component;
+    use token::components::token::erc721::erc721_enumerable::erc721_enumerable_component;
     use token::components::token::erc721::erc721_owner::erc721_owner_component;
 
     component!(path: src5_component, storage: src5, event: SRC5Event);
@@ -18,6 +19,9 @@ mod erc721_metadata_mock {
     );
     component!(
         path: erc721_mintable_component, storage: erc721_mintable, event: ERC721MintableEvent
+    );
+    component!(
+        path: erc721_enumerable_component, storage: erc721_enumerable, event: ERC721EnumerableEvent
     );
     component!(path: erc721_owner_component, storage: erc721_owner, event: ERC721OwnerEvent);
 
@@ -43,6 +47,8 @@ mod erc721_metadata_mock {
         #[substorage(v0)]
         erc721_mintable: erc721_mintable_component::Storage,
         #[substorage(v0)]
+        erc721_enumerable: erc721_enumerable_component::Storage,
+        #[substorage(v0)]
         erc721_owner: erc721_owner_component::Storage,
     }
 
@@ -54,6 +60,7 @@ mod erc721_metadata_mock {
         ERC721BalanceEvent: erc721_balance_component::Event,
         ERC721MetadataEvent: erc721_metadata_component::Event,
         ERC721MintableEvent: erc721_mintable_component::Event,
+        ERC721EnumerableEvent: erc721_enumerable_component::Event,
         ERC721OwnerEvent: erc721_owner_component::Event
     }
 }

token/src/components/tests/mocks/erc721/erc721_mintable_burnable_mock.cairo
@@ -7,6 +7,7 @@ mod erc721_mintable_burnable_mock {
     use token::components::token::erc721::erc721_metadata_hooks::ERC721MetadataHooksEmptyImpl;
     use token::components::token::erc721::erc721_mintable::erc721_mintable_component;
     use token::components::token::erc721::erc721_burnable::erc721_burnable_component;
+    use token::components::token::erc721::erc721_enumerable::erc721_enumerable_component;
     use token::components::token::erc721::erc721_owner::erc721_owner_component;
 
     component!(path: src5_component, storage: src5, event: SRC5Event);
@@ -22,6 +23,9 @@ mod erc721_mintable_burnable_mock {
     );
     component!(
         path: erc721_burnable_component, storage: erc721_burnable, event: ERC721BurnableEvent
+    );
+    component!(
+        path: erc721_enumerable_component, storage: erc721_enumerable, event: ERC721EnumerableEvent
     );
     component!(path: erc721_owner_component, storage: erc721_owner, event: ERC721OwnerEvent);
 
@@ -47,6 +51,8 @@ mod erc721_mintable_burnable_mock {
         #[substorage(v0)]
         erc721_burnable: erc721_burnable_component::Storage,
         #[substorage(v0)]
+        erc721_enumerable: erc721_enumerable_component::Storage,
+        #[substorage(v0)]
         erc721_owner: erc721_owner_component::Storage,
     }
 
@@ -59,6 +65,7 @@ mod erc721_mintable_burnable_mock {
         ERC721MetadataEvent: erc721_metadata_component::Event,
         ERC721MintableEvent: erc721_mintable_component::Event,
         ERC721BurnableEvent: erc721_burnable_component::Event,
+        ERC721EnumerableEvent: erc721_enumerable_component::Event,
         ERC721OwnerEvent: erc721_owner_component::Event
     }
 }

token/src/components/token/erc721/erc721_balance.cairo
@@ -58,14 +58,23 @@ mod erc721_balance_component {
     use dojo::world::{
         IWorldProvider, IWorldProviderDispatcher, IWorldDispatcher, IWorldDispatcherTrait
     };
-    use token::components::introspection::src5::{ISRC5Dispatcher, ISRC5DispatcherTrait};
+    
     use token::components::token::erc721::erc721_approval::erc721_approval_component as erc721_approval_comp;
     use token::components::token::erc721::erc721_owner::erc721_owner_component as erc721_owner_comp;
-    use token::components::token::erc721::interface::{
-        IERC721_RECEIVER_ID, IERC721ReceiverDispatcher, IERC721ReceiverDispatcherTrait
-    };
+    use token::components::token::erc721::erc721_enumerable::erc721_enumerable_component as erc721_enumerable_comp;
     use erc721_approval_comp::InternalImpl as ERC721ApprovalInternal;
     use erc721_owner_comp::InternalImpl as ERC721OwnerInternal;
+    use erc721_enumerable_comp::InternalImpl as ERC721EnumerableInternal;
+
+    use token::components::introspection::src5::{
+        ISRC5Dispatcher, ISRC5DispatcherTrait,
+        src5_component, src5_component::SRC5,
+    };    
+    use token::components::token::erc721::interface::{
+        IERC721ReceiverDispatcher, IERC721ReceiverDispatcherTrait,
+        IERC721_RECEIVER_ID, IERC721_ENUMERABLE_ID,
+    };
+
 
     #[storage]
     struct Storage {}
@@ -98,6 +107,8 @@ mod erc721_balance_component {
         +IWorldProvider<TContractState>,
         impl ERC721Approval: erc721_approval_comp::HasComponent<TContractState>,
         impl ERC721Owner: erc721_owner_comp::HasComponent<TContractState>,
+        impl ERC721Enumerable: erc721_enumerable_comp::HasComponent<TContractState>,
+        impl SRC5: src5_component::HasComponent<TContractState>,
         +Drop<TContractState>
     > of IERC721Balance<ComponentState<TContractState>> {
         fn balance_of(self: @ComponentState<TContractState>, account: ContractAddress) -> u256 {
@@ -144,6 +155,8 @@ mod erc721_balance_component {
         +IWorldProvider<TContractState>,
         impl ERC721Approval: erc721_approval_comp::HasComponent<TContractState>,
         impl ERC721Owner: erc721_owner_comp::HasComponent<TContractState>,
+        impl ERC721Enumerable: erc721_enumerable_comp::HasComponent<TContractState>,
+        impl SRC5: src5_component::HasComponent<TContractState>,
         +Drop<TContractState>
     > of IERC721BalanceCamel<ComponentState<TContractState>> {
         fn balanceOf(self: @ComponentState<TContractState>, account: ContractAddress) -> u256 {
@@ -177,6 +190,8 @@ mod erc721_balance_component {
         +IWorldProvider<TContractState>,
         impl ERC721Approval: erc721_approval_comp::HasComponent<TContractState>,
         impl ERC721Owner: erc721_owner_comp::HasComponent<TContractState>,
+        impl ERC721Enumerable: erc721_enumerable_comp::HasComponent<TContractState>,
+        impl SRC5: src5_component::HasComponent<TContractState>,
         +Drop<TContractState>
     > of InternalTrait<TContractState> {
         fn get_balance(
@@ -223,6 +238,12 @@ mod erc721_balance_component {
 
             erc721_owner.set_owner(token_id, to);
 
+            let src5 = get_dep_component!(@self, SRC5);
+            if src5.supports_interface(IERC721_ENUMERABLE_ID) {
+                let mut erc721_enumerable = get_dep_component_mut!(ref self, ERC721Enumerable);
+                erc721_enumerable.after_transfer(from, to, token_id);
+            }
+
             let transfer_event = Transfer { from, to, token_id };
 
             self.emit(transfer_event.clone());
@@ -251,7 +272,6 @@ mod erc721_balance_component {
             data: Span<felt252>
         ) -> bool {
             let src5_dispatcher = ISRC5Dispatcher { contract_address: to };
-
             if src5_dispatcher.supports_interface(IERC721_RECEIVER_ID) {
                 let erc721_dispatcher = IERC721ReceiverDispatcher { contract_address: to };
                 erc721_dispatcher

token/src/components/token/erc721/erc721_mintable.cairo
@@ -9,8 +9,10 @@ mod erc721_mintable_component {
         IWorldProvider, IWorldProviderDispatcher, IWorldDispatcher, IWorldDispatcherTrait
     };
 
+    use token::components::introspection::src5::src5_component as src5_comp;
     use token::components::token::erc721::erc721_approval::erc721_approval_component as erc721_approval_comp;
     use token::components::token::erc721::erc721_balance::erc721_balance_component as erc721_balance_comp;
+    use token::components::token::erc721::erc721_enumerable::erc721_enumerable_component as erc721_enumerable_comp;
     use token::components::token::erc721::erc721_owner::erc721_owner_component as erc721_owner_comp;
 
     use erc721_approval_comp::InternalImpl as ERC721ApprovalInternal;
@@ -34,6 +36,8 @@ mod erc721_mintable_component {
         impl ERC721Approval: erc721_approval_comp::HasComponent<TContractState>,
         impl ERC721Balance: erc721_balance_comp::HasComponent<TContractState>,
         impl ERC721Owner: erc721_owner_comp::HasComponent<TContractState>,
+        impl ERC721Enumerable: erc721_enumerable_comp::HasComponent<TContractState>,
+        impl SRC5: src5_comp::HasComponent<TContractState>,
         +Drop<TContractState>,
     > of InternalTrait<TContractState> {
         fn mint(ref self: ComponentState<TContractState>, to: ContractAddress, token_id: u256) {

Matth26/origami-roger

crates/token/src/components/tests/mocks/erc721/erc721_balance_mock.cairo
@@ -46,6 +46,7 @@ mod erc721_balance_mock {
     use origami_token::components::token::erc721::erc721_approval::erc721_approval_component;
     use origami_token::components::token::erc721::erc721_balance::erc721_balance_component;
     use origami_token::components::token::erc721::erc721_mintable::erc721_mintable_component;
+    use origami_token::components::token::erc721::erc721_enumerable::erc721_enumerable_component;
     use origami_token::components::token::erc721::erc721_owner::erc721_owner_component;
 
     component!(path: src5_component, storage: src5, event: SRC5Event);
@@ -55,6 +56,9 @@ mod erc721_balance_mock {
     component!(path: erc721_balance_component, storage: erc721_balance, event: ERC721BalanceEvent);
     component!(
         path: erc721_mintable_component, storage: erc721_mintable, event: ERC721MintableEvent
+    );
+    component!(
+        path: erc721_enumerable_component, storage: erc721_enumerable, event: ERC721EnumerableEvent
     );
     component!(path: erc721_owner_component, storage: erc721_owner, event: ERC721OwnerEvent);
 
@@ -93,6 +97,8 @@ mod erc721_balance_mock {
         #[substorage(v0)]
         erc721_mintable: erc721_mintable_component::Storage,
         #[substorage(v0)]
+        erc721_enumerable: erc721_enumerable_component::Storage,
+        #[substorage(v0)]
         erc721_owner: erc721_owner_component::Storage,
     }
 
@@ -103,6 +109,7 @@ mod erc721_balance_mock {
         ERC721ApprovalEvent: erc721_approval_component::Event,
         ERC721BalanceEvent: erc721_balance_component::Event,
         ERC721MintableEvent: erc721_mintable_component::Event,
+        ERC721EnumerableEvent: erc721_enumerable_component::Event,
         ERC721OwnerEvent: erc721_owner_component::Event,
     }
 

crates/token/src/components/tests/mocks/erc721/erc721_metadata_hooks_mock.cairo
@@ -16,6 +16,7 @@ mod erc721_metadata_hooks_mock {
     use origami_token::components::token::erc721::erc721_balance::erc721_balance_component;
     use origami_token::components::token::erc721::erc721_metadata::erc721_metadata_component;
     use origami_token::components::token::erc721::erc721_mintable::erc721_mintable_component;
+    use origami_token::components::token::erc721::erc721_enumerable::erc721_enumerable_component;
     use origami_token::components::token::erc721::erc721_owner::erc721_owner_component;
 
     component!(path: src5_component, storage: src5, event: SRC5Event);
@@ -28,6 +29,9 @@ mod erc721_metadata_hooks_mock {
     );
     component!(
         path: erc721_mintable_component, storage: erc721_mintable, event: ERC721MintableEvent
+    );
+    component!(
+        path: erc721_enumerable_component, storage: erc721_enumerable, event: ERC721EnumerableEvent
     );
     component!(path: erc721_owner_component, storage: erc721_owner, event: ERC721OwnerEvent);
 
@@ -53,6 +57,8 @@ mod erc721_metadata_hooks_mock {
         #[substorage(v0)]
         erc721_mintable: erc721_mintable_component::Storage,
         #[substorage(v0)]
+        erc721_enumerable: erc721_enumerable_component::Storage,
+        #[substorage(v0)]
         erc721_owner: erc721_owner_component::Storage,
     }
 
@@ -64,6 +70,7 @@ mod erc721_metadata_hooks_mock {
         ERC721BalanceEvent: erc721_balance_component::Event,
         ERC721MetadataEvent: erc721_metadata_component::Event,
         ERC721MintableEvent: erc721_mintable_component::Event,
+        ERC721EnumerableEvent: erc721_enumerable_component::Event,
         ERC721OwnerEvent: erc721_owner_component::Event
     }
 

crates/token/src/components/tests/mocks/erc721/erc721_metadata_mock.cairo
@@ -6,6 +6,7 @@ mod erc721_metadata_mock {
     use origami_token::components::token::erc721::erc721_metadata::erc721_metadata_component;
     use origami_token::components::token::erc721::erc721_metadata_hooks::ERC721MetadataHooksEmptyImpl;
     use origami_token::components::token::erc721::erc721_mintable::erc721_mintable_component;
+    use origami_token::components::token::erc721::erc721_enumerable::erc721_enumerable_component;
     use origami_token::components::token::erc721::erc721_owner::erc721_owner_component;
 
     component!(path: src5_component, storage: src5, event: SRC5Event);
@@ -18,6 +19,9 @@ mod erc721_metadata_mock {
     );
     component!(
         path: erc721_mintable_component, storage: erc721_mintable, event: ERC721MintableEvent
+    );
+    component!(
+        path: erc721_enumerable_component, storage: erc721_enumerable, event: ERC721EnumerableEvent
     );
     component!(path: erc721_owner_component, storage: erc721_owner, event: ERC721OwnerEvent);
 
@@ -43,6 +47,8 @@ mod erc721_metadata_mock {
         #[substorage(v0)]
         erc721_mintable: erc721_mintable_component::Storage,
         #[substorage(v0)]
+        erc721_enumerable: erc721_enumerable_component::Storage,
+        #[substorage(v0)]
         erc721_owner: erc721_owner_component::Storage,
     }
 
@@ -54,6 +60,7 @@ mod erc721_metadata_mock {
         ERC721BalanceEvent: erc721_balance_component::Event,
         ERC721MetadataEvent: erc721_metadata_component::Event,
         ERC721MintableEvent: erc721_mintable_component::Event,
+        ERC721EnumerableEvent: erc721_enumerable_component::Event,
         ERC721OwnerEvent: erc721_owner_component::Event
     }
 }

crates/token/src/components/tests/mocks/erc721/erc721_mintable_burnable_mock.cairo
@@ -7,6 +7,7 @@ mod erc721_mintable_burnable_mock {
     use origami_token::components::token::erc721::erc721_metadata_hooks::ERC721MetadataHooksEmptyImpl;
     use origami_token::components::token::erc721::erc721_mintable::erc721_mintable_component;
     use origami_token::components::token::erc721::erc721_burnable::erc721_burnable_component;
+    use origami_token::components::token::erc721::erc721_enumerable::erc721_enumerable_component;
     use origami_token::components::token::erc721::erc721_owner::erc721_owner_component;
 
     component!(path: src5_component, storage: src5, event: SRC5Event);
@@ -22,6 +23,9 @@ mod erc721_mintable_burnable_mock {
     );
     component!(
         path: erc721_burnable_component, storage: erc721_burnable, event: ERC721BurnableEvent
+    );
+    component!(
+        path: erc721_enumerable_component, storage: erc721_enumerable, event: ERC721EnumerableEvent
     );
     component!(path: erc721_owner_component, storage: erc721_owner, event: ERC721OwnerEvent);
 
@@ -47,6 +51,8 @@ mod erc721_mintable_burnable_mock {
         #[substorage(v0)]
         erc721_burnable: erc721_burnable_component::Storage,
         #[substorage(v0)]
+        erc721_enumerable: erc721_enumerable_component::Storage,
+        #[substorage(v0)]
         erc721_owner: erc721_owner_component::Storage,
     }
 
@@ -59,6 +65,7 @@ mod erc721_mintable_burnable_mock {
         ERC721MetadataEvent: erc721_metadata_component::Event,
         ERC721MintableEvent: erc721_mintable_component::Event,
         ERC721BurnableEvent: erc721_burnable_component::Event,
+        ERC721EnumerableEvent: erc721_enumerable_component::Event,
         ERC721OwnerEvent: erc721_owner_component::Event
     }
 }

crates/token/src/components/token/erc721/erc721_balance.cairo
@@ -61,11 +61,23 @@ mod erc721_balance_component {
     use origami_token::components::introspection::src5::{ISRC5Dispatcher, ISRC5DispatcherTrait};
     use origami_token::components::token::erc721::erc721_approval::erc721_approval_component as erc721_approval_comp;
     use origami_token::components::token::erc721::erc721_owner::erc721_owner_component as erc721_owner_comp;
+    use origami_token::components::token::erc721::erc721_enumerable::erc721_enumerable_component as erc721_enumerable_comp;
     use origami_token::components::token::erc721::interface::{
         IERC721_RECEIVER_ID, IERC721ReceiverDispatcher, IERC721ReceiverDispatcherTrait
     };
     use erc721_approval_comp::InternalImpl as ERC721ApprovalInternal;
     use erc721_owner_comp::InternalImpl as ERC721OwnerInternal;
+    use erc721_enumerable_comp::InternalImpl as ERC721EnumerableInternal;
+
+    use origami_token::components::introspection::src5::{
+        ISRC5Dispatcher, ISRC5DispatcherTrait,
+        src5_component, src5_component::SRC5,
+    };    
+    use origami_token::components::token::erc721::interface::{
+        IERC721ReceiverDispatcher, IERC721ReceiverDispatcherTrait,
+        IERC721_RECEIVER_ID, IERC721_ENUMERABLE_ID,
+    };
+
 
     #[storage]
     struct Storage {}
@@ -98,6 +110,8 @@ mod erc721_balance_component {
         +IWorldProvider<TContractState>,
         impl ERC721Approval: erc721_approval_comp::HasComponent<TContractState>,
         impl ERC721Owner: erc721_owner_comp::HasComponent<TContractState>,
+        impl ERC721Enumerable: erc721_enumerable_comp::HasComponent<TContractState>,
+        impl SRC5: src5_component::HasComponent<TContractState>,
         +Drop<TContractState>
     > of IERC721Balance<ComponentState<TContractState>> {
         fn balance_of(self: @ComponentState<TContractState>, account: ContractAddress) -> u256 {
@@ -144,6 +158,8 @@ mod erc721_balance_component {
         +IWorldProvider<TContractState>,
         impl ERC721Approval: erc721_approval_comp::HasComponent<TContractState>,
         impl ERC721Owner: erc721_owner_comp::HasComponent<TContractState>,
+        impl ERC721Enumerable: erc721_enumerable_comp::HasComponent<TContractState>,
+        impl SRC5: src5_component::HasComponent<TContractState>,
         +Drop<TContractState>
     > of IERC721BalanceCamel<ComponentState<TContractState>> {
         fn balanceOf(self: @ComponentState<TContractState>, account: ContractAddress) -> u256 {
@@ -177,6 +193,8 @@ mod erc721_balance_component {
         +IWorldProvider<TContractState>,
         impl ERC721Approval: erc721_approval_comp::HasComponent<TContractState>,
         impl ERC721Owner: erc721_owner_comp::HasComponent<TContractState>,
+        impl ERC721Enumerable: erc721_enumerable_comp::HasComponent<TContractState>,
+        impl SRC5: src5_component::HasComponent<TContractState>,
         +Drop<TContractState>
     > of InternalTrait<TContractState> {
         fn get_balance(
@@ -188,7 +206,7 @@ mod erc721_balance_component {
         }
 
         fn set_balance(
-            self: @ComponentState<TContractState>, account: ContractAddress, amount: u256
+            ref self: ComponentState<TContractState>, account: ContractAddress, amount: u256
         ) {
             set!(
                 self.get_contract().world(),
@@ -223,6 +241,12 @@ mod erc721_balance_component {
 
             erc721_owner.set_owner(token_id, to);
 
+            let src5 = get_dep_component!(@self, SRC5);
+            if src5.supports_interface(IERC721_ENUMERABLE_ID) {
+                let mut erc721_enumerable = get_dep_component_mut!(ref self, ERC721Enumerable);
+                erc721_enumerable.after_transfer(from, to, token_id);
+            }
+
             let transfer_event = Transfer { from, to, token_id };
 
             self.emit(transfer_event.clone());
@@ -251,7 +275,6 @@ mod erc721_balance_component {
             data: Span<felt252>
         ) -> bool {
             let src5_dispatcher = ISRC5Dispatcher { contract_address: to };
-
             if src5_dispatcher.supports_interface(IERC721_RECEIVER_ID) {
                 let erc721_dispatcher = IERC721ReceiverDispatcher { contract_address: to };
                 erc721_dispatcher

crates/token/src/components/token/erc721/erc721_mintable.cairo
@@ -9,8 +9,10 @@ mod erc721_mintable_component {
         IWorldProvider, IWorldProviderDispatcher, IWorldDispatcher, IWorldDispatcherTrait
     };
 
+    use origami_token::components::introspection::src5::src5_component as src5_comp;
     use origami_token::components::token::erc721::erc721_approval::erc721_approval_component as erc721_approval_comp;
     use origami_token::components::token::erc721::erc721_balance::erc721_balance_component as erc721_balance_comp;
+    use origami_token::components::token::erc721::erc721_enumerable::erc721_enumerable_component as erc721_enumerable_comp;
     use origami_token::components::token::erc721::erc721_owner::erc721_owner_component as erc721_owner_comp;
 
     use erc721_approval_comp::InternalImpl as ERC721ApprovalInternal;
@@ -34,6 +36,8 @@ mod erc721_mintable_component {
         impl ERC721Approval: erc721_approval_comp::HasComponent<TContractState>,
         impl ERC721Balance: erc721_balance_comp::HasComponent<TContractState>,
         impl ERC721Owner: erc721_owner_comp::HasComponent<TContractState>,
+        impl ERC721Enumerable: erc721_enumerable_comp::HasComponent<TContractState>,
+        impl SRC5: src5_comp::HasComponent<TContractState>,
         +Drop<TContractState>,
     > of InternalTrait<TContractState> {
         fn mint(ref self: ComponentState<TContractState>, to: ContractAddress, token_id: u256) {